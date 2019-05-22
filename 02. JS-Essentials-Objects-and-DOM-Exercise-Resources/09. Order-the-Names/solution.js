05ebc738508d4e4260483b78a4f07eb782994ffb
Zhaniartt <46032415+Zhaniartt@users.noreply.github.com>
2019-05-23 00:11:58 +0300

    Update solution.js

--- 02. JS-Essentials-Objects-and-DOM-Exercise-Resources/09. Order-the-Names/solution.js
+++ 02. JS-Essentials-Objects-and-DOM-Exercise-Resources/09. Order-the-Names/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let input = document.querySelector('article input');
+    let button = document.querySelector('article button').addEventListener('click',addName);
 
-    //TODO...
+    function addName(){
+       let section = Array.from(document.querySelectorAll('ol li'));
+      let assciiValue = 65;
+      for(let sect of section){
+          sect.setAttribute('id',assciiValue)
+          assciiValue++
+      }
+      let currentName = input.value.toLowerCase();
+      let firstLetter = String(currentName[0].toUpperCase().charCodeAt(0))
+      currentName = currentName.charAt(0).toUpperCase() + currentName.slice(1)
+      if(document.getElementById(firstLetter).textContent === ''){
+         document.getElementById(firstLetter).textContent = [currentName]
+        }else{
+            let existing = [document.getElementById(firstLetter).textContent]
+           existing = existing[0].split(',')
+            existing.push(currentName) 
+           existing= existing.map(x=> x.trim())          
+            document.getElementById(firstLetter).innerHTML = existing.join(', ')
+
+        }
+        input.value = ''
+    }
+    
 }
